af161412ae43e9125907c21095726789b8b79fb7
Juliette MEDINE <juliette.medine@gmail.com>
2018-04-27 15:32:27 +0200

    Site aston

---
+++ website/js/main.js
@@ -0,0 +1,8 @@
+// pour inclure un fichier js dans la page, voir dans le html
+// alert est une fonction javascript
+var btn = document.getElementById('btnHello');
+
+btn.addEventListener('click', function() {
+    
+    alert('Hello, World');
+}, false);
